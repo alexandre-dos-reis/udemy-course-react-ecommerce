3e7824a79881f567486a3a7d29e784e2fa4be968
Alex <ajm.dosreis.daponte@gmail.com>
2021-12-23 21:06:15 +0100

    Cart Items now working

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import CartItem from "../cart-item/cart-item.component";
+import CustomButton from "../custom-button/custom-button.component";
+import "./cart-dropdown.style.scss";
+import { connect } from "react-redux";
+
+const CartDropdown = ({ cartItems }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+// Passing the destructuring state to cartItems prop.
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
